01e34b787a1a74d31e095a9144fba1289f7d2e35
Pibhu <pibhu.cht@student.mahidol.edu>
2025-02-06 15:28:42 +0700

    yes!

--- src/main/java/Project1/main.java
+++ src/main/java/Project1/main.java
@@ -3,6 +3,6 @@ package Project1;
 public class main {
     public static void main(String []args)
     {
-        System.out.println("hello world i am ninny");
+        System.out.println("hello world i am p");
     }
 }
