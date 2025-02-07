f008e4b23eda6f1cd4a15c099e24e9375689405a
hope <akkhrawin22@gmail.com>
2025-02-07 20:54:28 +0700

    Kod rock

--- src/main/java/Project1/main.java
+++ src/main/java/Project1/main.java
@@ -1,8 +1,9 @@
 package Project1;
 
 public class main {
-    public static void main(String []args)
-    {
+    public static void main(String[] args) {
         System.out.println("hello world i am p");
+
+        System.out.println("Hello GUROCK");
     }
 }
