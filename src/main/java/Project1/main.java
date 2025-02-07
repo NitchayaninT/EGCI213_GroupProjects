df2fe35648d68a34afbfdeb898d673a4be359629
Pakin49 <philgus884@gmail.com>
2025-02-07 21:27:22 +0700

    Draft

--- src/main/java/Project1/main.java
+++ src/main/java/Project1/main.java
@@ -1,8 +1,38 @@
 package Project1;
 
+import java.util.*;
+import java.io.*;
+
 public class main {
     public static void main(String []args)
     {
         System.out.println("hello world i am p");
+
+        // Read file From product.txt
+        File productFile = new File()
+        Scanner productScan
     }
+}
+
+class Product{
+    // Class variable
+    protected String product_code;
+    protected String name;
+    protected int unit_price;
+    protected static ArrayList <Integer> tol_sales_price = new ArrayList<>();
+    protected static ArrayList <Integer> tol_sales_unit = new ArrayList<>();
+
+    // Constructor
+    public Product(){ super();}
+    public Product(String pc, String name, int unit_price){
+        this.product_code = pc;
+        this.name = name;
+        this.unit_price = unit_price;
+    }
+
+    // Methods
+    public static void update_product(String pc, int units){
+
+    }
+
 }
