f98d2337f78a902a0dec29bf6bdeab88c3f118de
Pakin-pan <pakin.pan@student.mahidol.edu>
2025-02-08 12:33:12 +0700

    Class product

--- src/main/java/Project1/main.java
+++ src/main/java/Project1/main.java
@@ -19,20 +19,41 @@ class Product{
     protected String product_code;
     protected String name;
     protected int unit_price;
-    protected static ArrayList <Integer> tol_sales_price = new ArrayList<>();
-    protected static ArrayList <Integer> tol_sales_unit = new ArrayList<>();
+    // static HashMap to store total_sales of each products
+    protected static HashMap<String,Integer> tol_sales_price = new HashMap<>();
+    protected static HashMap<String,Integer> tol_sales_unit = new HashMap<>();
 
     // Constructor
-    public Product(){ super();}
+    public Product(){ super();} // default
     public Product(String pc, String name, int unit_price){
         this.product_code = pc;
         this.name = name;
         this.unit_price = unit_price;
+        // If it is the first time that this product is mention create in the HashMap
+        if(!tol_sales_price.containsKey(pc)){
+            tol_sales_price.put(pc,0);
+            tol_sales_unit.put(pc,0);
+        }
     }
 
     // Methods
-    public static void update_product(String pc, int units){
+    // update the total_sales value
+    public void record_sales(int unit){
+        // Update the price by unit*unit_price
+        tol_sales_price.put(this.product_code,tol_sales_price.get(product_code)+(unit*this.unit_price));
+        tol_sales_unit.put(this.product_code,tol_sales_unit.get(product_code)+1);
+    }
 
+    //Setter Getter
+    public int get_unit_price(){return this.unit_price;}
+    public String get_name(){return this.name;}
+    public String get_product_code(){return this.product_code;}
+    // Use product code to get total_sales
+    public int get_tol_sales_price(String pc){
+        return tol_sales_price.get(pc);
+    }
+    public int get_tol_sales_unit(String pc){
+        return tol_sales_unit.get(pc);
     }
 
 }
@@ -56,6 +77,8 @@ class Order
         this.id = i;
         this.customer = customer;
         this.product = product;
+        // this update the tol_sales
+        this.product.record_sales(unit);
         this.unit = unit;
         this.installment = installment;
     }
@@ -66,15 +89,108 @@ class Order
     public Installment getInstall(){return installment;}
 
 }
+
 class InvalidInputException extends Exception {
 
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
+
 public class main {
-    public static void main(String []args)
-    {
+    public static void main(String[] args) {
         System.out.println("hello world i am p");
     }
 
-    // Read file From product.txt
-    File productFile = new File()
-    Scanner productScan
+    Map<String, Product> productMap = new HashMap<>();
+    Map<Integer, Installment> installmentMap = new HashMap<>();
+    ArrayList<Order> orders = new ArrayList<>();
+
+    // function use to read file from products
+    public void readProduct(){
+        boolean done = false;
+        String path = "src/main/java/Project1/";
+        String file_name = "products.txt";
+        do {
+            String in_path = path + file_name;
+            System.out.println();
+
+            // Declare line here because using in catch
+            String line;
+            try {
+                File infile = new File(in_path);
+                Scanner scan = new Scanner(infile);
+
+                // Skip the first line of the file
+                scan.nextLine();
+
+                // Scan until there is no nextline
+                while (scan.hasNext()) {
+                    line = scan.nextLine();
+                    String[] col = line.split(",");
+                    String product_code = col[0];
+                    String name = col[1];
+                    int unit_price = Integer.parseInt(col[2].trim());
+                    // if unit_price negative throw exception
+                    if (unit_price < 0) {
+                        throw new InvalidInputException("For unit_price: \"" + unit_price + "\"");
+                    }
+                    // put in the Hashmap
+                    productMap.put(product_code, new Product(product_code, name, unit_price));
+                }
+                done = true;
+            } catch (InvalidInputException e) {
+                System.out.println(e);
+                System.out.printf("%s --> skip line\n\n", line);
+            } catch (FileNotFoundException e) {
+                System.err.println(e);
+                System.out.println("Enter correct file name =");
+                Scanner key_scan = new Scanner(System.in);
+                file_name = key_scan.nextLine();
+            }
+        }while(!done);
+    }
+
+    public void readOrder() {
+        String path = "src/main/Java/Project1/";
+        String fileName = "orders.txt";
+        String inputFile = path + fileName;
+        System.out.println("Read from " + inputFile);
+        try {
+            File infile = new File(inputFile);
+            Scanner scan = new Scanner(infile);
+            if (scan.hasNextLine()) {
+                scan.nextLine();  // Skip the first line (header)
+            }
+            while (scan.hasNext()) {
+                String line = "";
+                try {
+                    line = scan.nextLine();
+                    String[] col = line.split(",");
+                    int id = Integer.parseInt(col[0].trim   ());
+                    String name = col[1].trim();
+
+                    String code = col[2].trim();
+                    if (productMap.get(code) == null) {
+                        throw new InvalidInputException("For product: \"" + col[2].trim() + "\"");
+                    }
+
+                    int unit = Integer.parseInt(col[3].trim());
+                    if (unit < 0) {
+                        throw new InvalidInputException("For units: \"" + col[3].trim() + "\"");
+                    }
+                    int month = Integer.parseInt(col[4].trim());
+                    orders.add(new Order(id, new Customer(name), productMap.get(code), unit, installmentMap.get(month)));
+                } catch (Exception e) {
+                    System.out.println(e);
+                    System.out.printf("%s --> skip line\n\n", line);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.print(e + " --> ");
+            System.out.println("Enter correct file name = ");
+            Scanner keyboard = new Scanner(System.in);
+            fileName = keyboard.next();
+        }
+    }
 }
