fed0184682dcc3ed8482b0b106c946981a3a743a
hope <akkhrawin22@gmail.com>
2025-02-12 22:32:28 +0700

    siuuu

--- src/main/java/Project1/main.java
+++ src/main/java/Project1/main.java
@@ -118,26 +118,27 @@ public class main {
     public void readProduct(){
         boolean done = false;
         String path = "src/main/java/Project1/";
-        String file_name = "products.txt";
-        do {
-            String in_path = path + file_name;
-            System.out.println();
+        String file_name = "product.txt";
 
+        do {
             // Declare line here because using in catch
             String line = "";
             try {
+                String in_path = path + file_name;
                 File infile = new File(in_path);
                 Scanner scan = new Scanner(infile);
 
+                System.out.println("Read form "+in_path);
                 // Skip the first line of the file
-                scan.nextLine();
-
+                if(scan.hasNext()) {
+                    scan.nextLine();
+                }
                 // Scan until there is no nextline
                 while (scan.hasNext()) {
                     line = scan.nextLine();
                     String[] col = line.split(",");
-                    String product_code = col[0];
-                    String name = col[1];
+                    String product_code = col[0].trim();
+                    String name = col[1].trim();
                     int unit_price = Integer.parseInt(col[2].trim());
                     // if unit_price negative throw exception
                     if (unit_price < 0) {
@@ -151,104 +152,117 @@ public class main {
                 System.out.println(e);
                 System.out.printf("%s --> skip line\n\n", line);
             } catch (FileNotFoundException e) {
-                System.err.println(e);
+                System.out.println(e);
                 System.out.println("Enter correct file name =");
                 Scanner key_scan = new Scanner(System.in);
                 file_name = key_scan.nextLine();
+
             }
         }while(!done);
-    }
-
-    public void readOrder() {
-        String path = "src/main/Java/Project1/";
-        String fileName = "orders.txt";
-        String inputFile = path + fileName;
-        System.out.println("Read from " + inputFile);
-        try {
-            File infile = new File(inputFile);
-            Scanner scan = new Scanner(infile);
-            if (scan.hasNextLine()) {
-                scan.nextLine();  // Skip the first line (header)
-            }
-            while (scan.hasNext()) {
-                String line = "";
-                try
-                {
-                    line = scan.nextLine();
-                    String []col = line.split(",");
-                    int id = Integer.parseInt(col[0].trim());
-                    String name = col[1].trim();
-                    String code = col[2].trim();
-                    if(productMap.get(code)==null)
-                    {
-                        throw new InvalidInputException("For product: \"" + col[2].trim() + "\"");
-                    }
-
-                    int unit = Integer.parseInt(col[3].trim());
-
-                    if(unit<0)
-                    {
-                        throw new InvalidInputException("For units: \"" + col[3].trim() + "\"");
-                    }
-                    int month = Integer.parseInt(col[4].trim());
-                    orders.add(new Order(id,new Customer(name),productMap.get(code),unit,installmentMap.get(month)));
-                }catch(Exception e)
-                {
-                 System.out.println(e);
-                 System.out.printf("%s --> skip line\n\n", line);
-                }
-            }
-        } catch (FileNotFoundException e) {
-            System.out.print(e + " --> ");
-            System.out.println("Enter correct file name = ");
-            Scanner keyboard = new Scanner(System.in);
-            fileName = keyboard.next();
-        } 
-    }
-    public void readInstallments()
-    {
-        String path = "src/main/java/Project1/";
-        String fileName  = "installments.txt";
-        String inputFile = path+fileName;
-        System.out.println("Read from "+inputFile);
-        try{
-            File inFile = new File(inputFile);
-            Scanner fileScan = new Scanner(inFile);
-
-            int count = 0;
-            while(fileScan.hasNext())
-            {
-                String line = fileScan.nextLine();
-                if(count == 0)
-                {
-                    count++;
-                    continue;
-                }
-                try {
-                    String[] cols = line.split(",");
-                    int months = Integer.parseInt(cols[0].trim());
-
-                    double interest = Double.parseDouble(cols[1].trim());
-                    installmentMap.put(months,new Installment(months,interest));
-                    installmentMap.get(months).print();
-                    count++;
-                }
-                catch(RuntimeException e)//includes all the child classes of RuntimeException
-                {
-                    System.out.println(e.toString());
-                    System.out.printf("%s --> skip line\n\n", line);
-                    count++;
-                }
-            }
-
-        }catch(FileNotFoundException e)
-        {
-            System.out.print(e + " --> ");
-            System.out.println("Enter correct file name = ");
-            Scanner keyboard = new Scanner(System.in);
-            fileName = keyboard.next();
+        for (Map.Entry<String, Product> entry : productMap.entrySet()) {
+            Product product = entry.getValue();
+            System.out.printf("%s (%s) unit price %d\n", product.get_name(), product.get_product_code(), product.get_unit_price());
         }
     }
+
+    public void readInstallments() {
+        boolean done = false;
+        String path = "src/main/java/Project1/";
+        String fileName = "installment.txt";
+
+        while (!done) {
+
+            try {
+                String inputFile = path + fileName;
+                File inFile = new File(inputFile);
+                Scanner fileScan = new Scanner(inFile);
+
+                System.out.println("Read from " + inputFile);
+
+                int count = 0;
+                while (fileScan.hasNext()) {
+                    String line = fileScan.nextLine();
+                    if (count == 0) {
+                        count++;
+                        continue;
+                    }
+                    try {
+                        String[] cols = line.split(",");
+                        int months = Integer.parseInt(cols[0].trim());
+
+                        double interest = Double.parseDouble(cols[1].trim());
+                        installmentMap.put(months, new Installment(months, interest));
+                        installmentMap.get(months).print();
+                        count++;
+                    } catch (RuntimeException e)//includes all the child classes of RuntimeException
+                    {
+                        System.out.println(e.toString());
+                        System.out.printf("%s --> skip line\n\n", line);
+                        count++;
+                    }
+                } done = true;
+
+            } catch (FileNotFoundException e) {
+                System.out.println(e);
+                System.out.println("Enter correct file name = ");
+                Scanner keyboard = new Scanner(System.in);
+                fileName = keyboard.nextLine();
+            }
+        }
+    }
+    public void readOrder() {
+        boolean done = false;
+        String path = "src/main/Java/Project1/";
+        String fileName = "orders_error.txt";
+        while (!done) {
+            try {
+                String inputFile = path + fileName;
+                File infile = new File(inputFile);
+                Scanner scan = new Scanner(infile);
+                System.out.println("Read from " + inputFile);
+                if (scan.hasNextLine()) {
+                    scan.nextLine();  // Skip the first line (header)
+                }
+                while (scan.hasNext()) {
+                    String line = "";
+                    try {
+                        line = scan.nextLine();
+                        String[] col = line.split(",");
+
+                        int id = Integer.parseInt(col[0].trim());
+                        String name = col[1].trim();
+                        String code = col[2].trim();
+                        if (productMap.get(code) == null) {
+                            throw new InvalidInputException("For product: \"" + col[2].trim() + "\"");
+                        }
+
+                        int unit = Integer.parseInt(col[3].trim());
+
+                        if (unit < 0) {
+                            throw new InvalidInputException("For units: \"" + col[3].trim() + "\"");
+                        }
+                        int month = Integer.parseInt(col[4].trim());
+
+                        if (installmentMap.get(month) == (null)) {
+                            throw new InvalidInputException("For installment plan: \"" + col[4].trim() + "\"");
+                        }
+
+                        orders.add(new Order(id, new Customer(name), productMap.get(code), unit, installmentMap.get(month)));
+                    } catch (Exception e) {
+                        System.out.println(e);
+                        System.out.printf("%s --> skip line\n\n", line);
+                    }
+                }
+                done = true;
+            } catch (FileNotFoundException e) {
+                System.out.println(e);
+                System.out.println("Enter correct file name = ");
+                Scanner keyboard = new Scanner(System.in);
+                fileName = keyboard.nextLine();
+            }
+        }
+    }
+
     public void processOrder()
     {
         //read from ArrayList of orders
