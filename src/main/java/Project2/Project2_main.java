bf934add978b0c5ab601d46370b05c07c4d10d85
NitchayaninT <nitchayanin.tha@student.mahidol.edu>
2025-03-01 16:05:23 +0700

    added java file for project 2

---
+++ src/main/java/Project2/Project2_main.java
@@ -0,0 +1,15 @@
+//Akkhrawin Nair 6580013
+//Pakin Panawattanakul 6580043
+//Nitchayanin Thamkunanon 6580081
+//Pibhu Chitburanachart 6580195
+//Panupong Sangaphunchai 6580587
+package Project2;
+
+
+//main class
+public class Project2_main {
+    public static void main(String []args)
+    {
+        System.out.println("hello");
+    }
+}
---
+++ src/main/java/Project2/README.txt
@@ -0,0 +1,5 @@
+Akkhrawin Nair 6580013
+Pakin Panawattanakul 6580043
+Nitchayanin Thamkunanon 6580081
+Pibhu Chitburanachart 6580195
+Panupong Sangaphunchai 6580587
