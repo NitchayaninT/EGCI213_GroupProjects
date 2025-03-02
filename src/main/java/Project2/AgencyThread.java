0cb3811d9a9e8c9c22128069c3009997563daa77
NitchayaninT <nitchayanin.tha@student.mahidol.edu>
2025-03-02 19:17:37 +0700

    did something in main and AgencyThread, can try running. it just prints day 1 info

---
+++ src/main/java/Project2/AgencyThread.java
@@ -0,0 +1,46 @@
+package Project2;
+
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+//individual travel agency
+//each iteration of a loop = 1 day
+public class AgencyThread extends Thread{
+    private int remaining_cus;
+    private final int max_arrival;
+    private CyclicBarrier barrier;
+    //private ArrayList<Tour> tours
+
+    //constructor
+    AgencyThread(String name, int max){super(name); this.max_arrival=max;}
+
+    //setters
+    public void setBarrier(CyclicBarrier ba) { barrier = ba; }
+
+    //run method for each agency thread
+    public void run()
+    {
+        //receive customers and update remaining customers(from prev days + today)
+        Random rand = new Random();
+        int arriving_cus = rand.nextInt(max_arrival);
+        remaining_cus+=arriving_cus;
+
+        //wait for main to print day
+        try {
+            barrier.await();
+        } catch (Exception e) {}
+
+        //printing new arrival and remaining customers
+        printNewArrival(arriving_cus);
+        try {
+            barrier.await(); //threads waiting at the barrier before printing next thing
+        } catch (Exception e) {}
+
+        //printing send xx customers to Tour ...
+    }
+    private void printNewArrival(int arriving)
+    {
+        System.out.printf("%17s" + " >> new arrival = "+arriving+"%15s remaining customers = "+remaining_cus+"\n",Thread.currentThread().getName(),"");
+    }
+}
--- src/main/java/Project2/Project2_main.java
+++ src/main/java/Project2/Project2_main.java
@@ -5,11 +5,103 @@
 //Panupong Sangaphunchai 6580587
 package Project2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.concurrent.CyclicBarrier;
 
 //main class
 public class Project2_main {
+    int days_simulation; //days of simulation
+    ArrayList<AgencyThread> AgencyThreads;//array of Agency threads
+    CyclicBarrier barrier; //barrier for agency threads + main (for operation thread, pls create yourself)
+
+    //MAIN METHOD
     public static void main(String []args)
     {
-        System.out.println("hello");
+        Project2_main program = new Project2_main();
+        program.readSimulation();
+        try{
+            program.runSimulation();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    //method to read from file
+    public void readSimulation()
+    {
+        boolean done = false;
+        String path = "src/main/java/Project2/";
+        String file_name = "config.txt";
+        //initialized
+        AgencyThreads = new ArrayList<>();
+
+        do {
+            try {
+                String in_path = path + file_name;
+                File infile = new File(in_path);
+                Scanner scan = new Scanner(infile);
+
+                //reading from file
+                int lineCount = 0;
+                while (scan.hasNext()) {
+                    String line = scan.nextLine();
+                    String []cols = line.split(",");
+                    if(lineCount==0) { //line 0 = for number of days
+                        days_simulation = Integer.parseInt(cols[1].trim());
+                    }
+                    else if (lineCount==1) { //line 1 = for agencies
+                        int agency_num = Integer.parseInt(cols[1].trim());
+                        int max_daily_arrival = Integer.parseInt(cols[2].trim());
+                        barrier = new CyclicBarrier( agency_num+1 ); //barrier for all agency threads + main
+                        for(int i=0;i<agency_num;i++)
+                        {
+                            String agency_name = "AgencyThread_"+Integer.toString(i);
+                            AgencyThreads.add(new AgencyThread(agency_name,max_daily_arrival));//create thread object
+                        }
+                        for (AgencyThread A: AgencyThreads) {A.setBarrier( barrier );}//set barrier to all threads
+                    }
+                    else if(lineCount==2){//line 2 = for tours
+                        int tour_num = Integer.parseInt(cols[1].trim());
+                        int tour_capacity = Integer.parseInt(cols[2].trim());
+                    }
+                    else if(lineCount==3){//line 3 = for places
+                        int place_num = Integer.parseInt(cols[1].trim());
+                    }
+                    lineCount++;
+                }
+                done = true;
+            } catch (FileNotFoundException e) {
+                System.out.println();
+                System.out.println(e);
+                System.out.println("New file name =");
+                Scanner key_scan = new Scanner(System.in);
+                file_name = key_scan.nextLine();
+            }
+            catch(Exception e)
+            {
+                System.out.println(e.toString());
+            }
+
+        }while(!done);
+    }
+    //method to run simulation
+    public void runSimulation() throws Exception {
+        for(AgencyThread A : AgencyThreads) {A.start();} //start agency threads
+
+        for(int i=1;i<= days_simulation;i++)//start simulation from day 1
+        {
+            System.out.printf("%17s"+ " >> "+"=".repeat(60)+"\n",Thread.currentThread().getName());
+            System.out.printf("%17s"+ " >> "+"Day%2d\n",Thread.currentThread().getName(),i);
+
+            barrier.await(); //check point for other threads to start
+            //threads printing stuff...
+
+            for(AgencyThread A: AgencyThreads){
+                A.join();
+            }
+        }
     }
 }
+    
