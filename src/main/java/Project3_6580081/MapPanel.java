b4be7ad346e4d40197ec341a8f4c3bd021fcec94
NitchayaninT <nitchayanin.tha@student.mahidol.edu>
2025-03-25 10:33:57 +0700

    about to send

--- src/main/java/Project3_6580081/MapPanel.java
+++ src/main/java/Project3_6580081/MapPanel.java
@@ -1,4 +1,9 @@
-package Project3;
+/*Akkhrawin Nair 6580013
+Pakin Panawattanakul 6580043
+Nitchayanin Thamkunanon 6580081
+Pibhu Chitburanachart 6580195
+Panupong Sangaphunchai 6580587*/
+package Project3_6580081;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
